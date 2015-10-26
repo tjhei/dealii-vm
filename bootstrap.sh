2901d77816acc3420583877049d33032323cb35e
Timo Heister <timo.heister@gmail.com>
2015-10-26 11:22:48 -0400

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 ls /vagrant/lib-setup.sh || { echo "vagrant mount not working";exit 1; }
 
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 
 sudo apt-get install -y xubuntu-desktop
 
@@ -13,11 +13,13 @@ sudo apt-get install -y xubuntu-desktop
 sudo /usr/share/debconf/fix_db.pl
 sudo dpkg --configure -a
 
-sudo apt-get remove -y gnumeric abiword xscreensaver  thunderbird xchat xfburn  gnome-sudoku gnome-mines abiword-common xchat-common gnome-screensaver gnumeric-common gnumeric-doc libabiword-3.0
+sudo apt-get remove -y gnumeric abiword xscreensaver  thunderbird xchat xfburn  gnome-sudoku gnome-mines abiword-common xchat-common gnome-screensaver gnumeric-common gnumeric-doc libabiword-3.0 xchat pidgin 
 
-apt-get install -y xfce4-terminal firefox eclipse-cdt
+apt-get install -y 
 
-apt-get install -y git emacs zile subversion git-cola gitk meld gdb valgrind g++    gfortran \
+apt-get install -y \
+    xfce4-terminal firefox eclipse-cdt git emacs zile subversion git-cola gitk meld gdb \
+    valgrind g++ gfortran \
     libblas-dev \
     liblapack-dev \
     libopenmpi-dev \
@@ -26,7 +28,7 @@ apt-get install -y git emacs zile subversion git-cola gitk meld gdb valgrind g++
     wget \
     python \
     numdiff \
-    leafpad gnuplot ninja-build  htop paraview
+    leafpad gnuplot ninja-build htop paraview
 
 
 echo ubuntu:ubuntu | sudo chpasswd
@@ -35,9 +37,12 @@ echo -e "[SeatDefaults]\nautologin-user=ubuntu\nautologin-user-timeout=0" | sudo
 
 echo "dealvm" | sudo tee /etc/hostname
 
+sudo startx
+
 #base:
 cd /home/ubuntu
 sudo -u ubuntu cp /vagrant/bash_aliases /home/ubuntu/.bash_aliases
+sudo -u ubuntu mkdir -p /home/ubuntu/Desktop/
 sudo -u ubuntu cp /vagrant/info.html /home/ubuntu/Desktop/
 sudo -u ubuntu mkdir -p bin
 sudo -u ubuntu rmdir Pictures Music Public Templates Videos >/dev/null 2>&1
@@ -69,6 +74,7 @@ sudo -u ubuntu chmod u+x /home/ubuntu/deal.II/setup.sh
 if [ ! -d "/home/ubuntu/aspect/.git" ]; then
   echo "installing aspect"
   sudo -u ubuntu mkdir aspect
+  echo `pwd`
   sudo -u ubuntu git clone https://github.com/geodynamics/aspect.git
 else
  cd /home/ubuntu/aspect
@@ -80,12 +86,3 @@ sudo -u ubuntu cp /vagrant/aspect-setup.sh /home/ubuntu/aspect/_setup.sh
 sudo -u ubuntu chmod u+x /home/ubuntu/aspect/_setup.sh
 
 
-#sudo reboot
-
-# before release:
-# apt-get autoclean
-# reboot into recovery, mount -o remount,rw /, zerofree
-# sudo mount -o ro -t ext4 /dev/sda1 /bla
-# zerofree -v /dev/sda1
-# https://wiki.ubuntu.com/RecoveryMode
-# 
