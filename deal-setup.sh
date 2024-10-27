2bd091750e5e2d3c452ffe1199fa1e0e7ed3f875
Timo Heister <timo.heister@gmail.com>
2024-10-26 20:49:45 -0400

    new candi and deal.II

--- deal-setup.sh
+++ deal-setup.sh
@@ -7,7 +7,7 @@ rm -rf build installed
 if [ ! -d "dealii/.git" ]; then
   rm -rf dealii
   git clone https://github.com/dealii/dealii.git
-  cd dealii && git checkout v9.3.3 && cd ..
+  cd dealii && git checkout v9.6.0 && cd ..
 fi
 
 
@@ -20,6 +20,6 @@ mkdir build && cd build && \
           -D DEAL_II_WITH_TRILINOS=ON \
           -D DEAL_II_WITH_HDF5=ON \
           ../dealii && \
-    make -j2 && make install && \
+    make -j4 && make install && \
     make clean &&
     cd .. #&& rm -rf build
